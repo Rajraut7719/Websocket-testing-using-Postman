0179f305c63d255f87a6068e84d3bbb5d2095160
unknown <baburaut7719@gmail.com>
2022-07-01 12:14:51 +0530

    first commot

---
+++ demo2/asgi.py
@@ -0,0 +1,21 @@
+"""
+ASGI config for demo2 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter,URLRouter
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demo2.settings')
+import pp.routing
+application =ProtocolTypeRouter({
+    'http':get_asgi_application(),
+    'websocket':URLRouter(
+        pp.routing.websocket_urlpatterns
+    )
+})
